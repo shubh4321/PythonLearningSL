4bb228965a9c0a1835227aff31228c1d42f3a944
shubh4321 <ershubham.kumar@gmail.com>
2024-07-08 08:21:47 +0530

    First Commit

---
+++ test2.py
@@ -0,0 +1,9 @@
+print("Q1. Capital of India: ")
+print("1. Hyderabad 2. Chennai 3. New Delhi 4. Mumbai")
+ans=int(input("Enter your choice: "))
+if(ans==3):
+	print("Your are correct")
+else:
+	print("Incorrect answer")
+
+input("Press enter to exit")
