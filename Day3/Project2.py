0c62c507db9b1f29df54f3eba3a2d99040ed40a9
shubh4321 <ershubham.kumar@gmail.com>
2024-08-19 11:28:38 +0530

    day3

---
+++ Day3/Project2.py
@@ -0,0 +1,9 @@
+n = int(input("Enter a number 1/2/3: "))
+if(n==1):
+    print("ONE")
+elif(n==2):
+    print("TWO")
+elif(n==3):
+    print("THREE")
+else:
+    print("Please enter valid number between 1 to 3")
